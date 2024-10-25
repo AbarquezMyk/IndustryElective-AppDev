a6bcc2ce20a033ad7065be9f0f4623754e739e36
Leilah Joy Baruc <leilahjoy.baruc@cit.edu>
2024-10-25 21:57:26 +0800

    Initial Commit of PaymentMethoid

--- caresync/src/components/PaymentMethod.jsx
+++ caresync/src/components/PaymentMethod.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from './img/logo.png';
 import dashboard from './img/dashboard.png';
