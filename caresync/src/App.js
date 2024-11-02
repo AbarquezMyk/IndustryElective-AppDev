895f96d2fac197fb2857232dab033b465f307482
Gianne Andrea Garcia <gianneandrea.garcia@cit.edu>
2024-11-02 13:48:58 +0800

    Initial COmmit of App.js

--- caresync/src/App.js
+++ caresync/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+//import Home from './components/Home';
 // import PaymentMethods from './components/PaymentMethod';
 import Calendar from './components/Calendar';
 // import OnlineForm from './components/OnlineForm';
 // import Patient from './components/Patient';
-import PatientProfileForm from './components/PatientProfileForm';
+//import PatientProfileForm from './components/PatientProfileForm';
 // import CreditCard from './components/CreditCard';
-import Login from './components/Login';
-import Register from './components/Register';
+//import Login from './components/Login';
+//import Register from './components/Register';
 // import AppointmentHistory from './components/AppointmentHistory';
 
 const App = () => {
