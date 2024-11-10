02f1453206ff65046bf7a480ccf096799407c759
Mykha Abarquez <mykha.abarquez@cit.edu>
2024-11-11 07:57:43 +0800

    Initial Commit of App.js

--- caresync/src/App.js
+++ caresync/src/App.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-//import Home from './components/Home';
-// import PaymentMethods from './components/PaymentMethod';
-// import Calendar from './components/Calendar';
-// import OnlineForm from './components/OnlineForm';
-// import Patient from './components/Patient';
-// import PatientProfileForm from './components/PatientProfileForm';
-// import CreditCard from './components/CreditCard';
-// import Login from './components/Login';
-// import Register from './components/Register';
-// import AppointmentHistory from './components/AppointmentHistory';
+import Home from './components/Home';
+import PaymentMethods from './components/PaymentMethod';
+import Calendar from './components/Calendar';
+import OnlineForm from './components/OnlineForm';
+import Patient from './components/Patient';
+import PatientProfileForm from './components/PatientProfileForm';
+import CreditCard from './components/CreditCard';
+import Login from './components/Login';
+import Register from './components/Register';
+import AppointmentHistory from './components/AppointmentHistory';
 import Doctor from './components/Doctor';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-      {/*<Route path="/" element={<Navigate to="/calendar" replace />} /> */}
-      {/* <Route path="/calendar" element={<Calendar />} /> */}
-      {/* <Route path="/home" element={<Home />} /> */}
-      <Route path="/" element={<Doctor />} />
+        {/* Default route to Home */}
+        <Route path="/" element={<Home />} />
 
-      {/* <PaymentMethods /> */}
-      {/* <Calendar /> */}
+        {/* Authentication routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Other application routes */}
+        <Route path="/payment-methods" element={<PaymentMethods />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/online-form" element={<OnlineForm />} />
+        <Route path="/patient" element={<Patient />} />
+        <Route path="/patient-profile" element={<PatientProfileForm />} />
+        <Route path="/credit-card" element={<CreditCard />} />
+        <Route path="/appointment-history" element={<AppointmentHistory />} />
+        <Route path="/doctor" element={<Doctor />} />
+
+        {/* Redirect any unknown routes to Home */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
